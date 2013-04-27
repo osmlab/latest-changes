204567d42a5b731261e7acab63c538db2e71c3ef
Tom MacWright <tom@macwright.org>
2013-04-27 13:18:26 -0500

    First

---
+++ js/leaflet-osm.js
@@ -0,0 +1,167 @@
+L.OSM = {};
+
+L.OSM.DataLayer = L.FeatureGroup.extend({
+  options: {
+    areaTags: ['area', 'building', 'leisure', 'tourism', 'ruins', 'historic', 'landuse', 'military', 'natural', 'sport'],
+    uninterestingTags: ['source', 'source_ref', 'source:ref', 'history', 'attribution', 'created_by', 'tiger:county', 'tiger:tlid', 'tiger:upload_uuid'],
+    styles: {}
+  },
+
+  initialize: function (xml, options) {
+    L.Util.setOptions(this, options);
+
+    L.FeatureGroup.prototype.initialize.call(this);
+
+    if (xml) {
+      this.addData(xml);
+    }
+  },
+
+  addData: function (features) {
+    if (!(features instanceof Array)) {
+      features = this.buildFeatures(features);
+    }
+
+    for (var i = 0; i < features.length; i++) {
+      var feature = features[i], layer;
+
+      if (feature.type === "node") {
+        layer = L.circleMarker(feature.latLng, this.options.styles.node);
+      } else {
+        var latLngs = new Array(feature.nodes.length);
+
+        for (var j = 0; j < feature.nodes.length; j++) {
+          latLngs[j] = feature.nodes[j].latLng;
+        }
+
+        if (this.isWayArea(feature)) {
+          latLngs.pop(); // Remove last == first.
+          layer = L.polygon(latLngs, this.options.styles.area);
+        } else {
+          layer = L.polyline(latLngs, this.options.styles.way);
+        }
+      }
+
+      layer.addTo(this);
+      layer.feature = feature;
+    }
+  },
+
+  buildFeatures: function (xml) {
+    var features = [],
+      nodes = L.OSM.getNodes(xml),
+      ways = L.OSM.getWays(xml, nodes);
+
+    for (var node_id in nodes) {
+      var node = nodes[node_id];
+      if (this.interestingNode(node, ways)) {
+        features.push(node);
+      }
+    }
+
+    for (var i = 0; i < ways.length; i++) {
+      var way = ways[i];
+      features.push(way);
+    }
+
+    return features;
+  },
+
+  isWayArea: function (way) {
+    if (way.nodes[0] != way.nodes[way.nodes.length - 1]) {
+      return false;
+    }
+
+    for (var key in way.tags) {
+      if (~this.options.areaTags.indexOf(key)) {
+        return true;
+      }
+    }
+
+    return false;
+  },
+
+  interestingNode: function (node, ways) {
+    var used = false;
+
+    for (var i = 0; i < ways.length; i++) {
+      if (ways[i].nodes.indexOf(node) >= 0) {
+        used = true;
+        break;
+      }
+    }
+
+    if (!used) {
+      return true;
+    }
+
+    for (var key in node.tags) {
+      if (this.options.uninterestingTags.indexOf(key) < 0) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+});
+
+L.Util.extend(L.OSM, {
+  getNodes: function (xml) {
+    var result = {};
+
+    var nodes = xml.getElementsByTagName("node");
+    for (var i = 0; i < nodes.length; i++) {
+      var node = nodes[i], id = node.getAttribute("id");
+      result[id] = {
+        id: id,
+        type: 'node',
+        latLng: L.latLng(node.getAttribute("lat"),
+                         node.getAttribute("lon"), true),
+        timestamp: node.getAttribute('timestamp'),
+        changeset: node.getAttribute('changeset'),
+        user: node.getAttribute('user'),
+        tags: this.getTags(node)
+      };
+    }
+
+    return result;
+  },
+
+  getWays: function (xml, nodes) {
+    var result = [];
+
+    var ways = xml.getElementsByTagName("way");
+    for (var i = 0; i < ways.length; i++) {
+      var way = ways[i], nds = way.getElementsByTagName("nd");
+
+      var way_object = {
+        id: way.getAttribute("id"),
+        type: "way",
+        nodes: new Array(nds.length),
+        timestamp: way.getAttribute('timestamp'),
+        changeset: way.getAttribute('changeset'),
+        user: way.getAttribute('user'),
+        tags: this.getTags(way)
+      };
+
+      for (var j = 0; j < nds.length; j++) {
+        way_object.nodes[j] = nodes[nds[j].getAttribute("ref")];
+      }
+
+      result.push(way_object);
+    }
+
+    return result;
+  },
+
+  getTags: function (xml) {
+    var result = {};
+
+    var tags = xml.getElementsByTagName("tag");
+    for (var j = 0; j < tags.length; j++) {
+      result[tags[j].getAttribute("k")] = tags[j].getAttribute("v");
+    }
+
+    return result;
+  }
+});
---
+++ js/site.js
@@ -0,0 +1,112 @@
+var map = L.map('map', {
+    minZoom: 14,
+    maxZoom: 18
+}).setView([37.7, -97.3], 15);
+
+L.tileLayer('http://a.tiles.mapbox.com/v3/tmcw.map-l1m85h7s/{z}/{x}/{y}.png')
+    .addTo(map);
+
+var locationFilter = new L.LocationFilter().addTo(map);
+
+function run() {
+    var bounds = locationFilter.getBounds();
+    locationFilter.disable();
+    d3.xml('http://www.openstreetmap.org/api/0.6/map?bbox=' +
+        bounds.getSouthWest().lng + ',' +
+        bounds.getSouthWest().lat + ',' +
+        bounds.getNorthEast().lng + ',' +
+        bounds.getNorthEast().lat // + ',' +
+        ).on('load', function(xml) {
+
+            var layer = new L.OSM.DataLayer(xml).addTo(map);
+
+            map.fitBounds(layer.getBounds());
+
+            var bytime = [];
+
+            layer.eachLayer(function(l) {
+                bytime.push({
+                    time: new Date(l.feature.timestamp),
+                    feature: l
+                });
+            });
+
+            layer.on('click', function(e) {
+                click({ feature: e.layer });
+            });
+
+            bytime.sort(function(a, b) {
+                return (+b.time) - (+a.time);
+            });
+
+            var datescale = d3.time.scale()
+                .domain(d3.extent(bytime.map(function(b) { return b.time; })))
+                .range([0, 1]);
+
+            var colint = d3.interpolateRgb('#000', '#f00');
+
+            var rl = d3.select('#results')
+                .selectAll('div.result')
+                .data(bytime)
+                .enter()
+                .append('div')
+                .attr('class', 'result')
+                .style('color', function(l) {
+                    return colint(datescale(l.time));
+                });
+
+            function click(d) {
+                rl.classed('active', function(_) {
+                    return d == _;
+                });
+                resetStyle();
+                map.fitBounds(d.feature.getBounds());
+                d.feature.setStyle({ color: '#0f0' });
+                if (d3.event) d3.event.preventDefault();
+            }
+
+           rl.append('a').classed('load', true).html('&larr; ').attr('href', '#')
+           .on('click', click);
+           rl.append('span').classed('deemphasize', true).text('edited ');
+
+           rl.append('span').text(function(d) {
+               return moment(d.time).format('MMMM Do YYYY, h:mm:ss a ');
+           });
+
+           rl.append('span').classed('deemphasize', true).text('by ');
+
+           rl.append('span').text(function(d) {
+               return d.feature.feature.user + ' ';
+           });
+
+           rl.append('a').attr('class', 'l changeset-link').text(function(d) {
+               return 'changeset';
+           })
+           .attr('target', '_blank')
+           .attr('href', function(d) {
+               return 'http://openstreetmap.org/browse/changeset/' + d.feature.feature.changeset;
+           });
+
+           rl.append('a').attr('class', 'l item-link').text(function(d) {
+               return d.feature.feature.type;
+           })
+           .attr('target', '_blank')
+           .attr('href', function(d) {
+               return 'http://openstreetmap.org/browse/' + d.feature.feature.type + '/' + d.feature.feature.id + '/history';
+           });
+
+           function resetStyle() {
+               layer.eachLayer(function(l) {
+                   l.setStyle({
+                       color: colint(datescale(new Date(l.feature.timestamp))),
+                       opacity: 0.8
+                   });
+               });
+           }
+
+           resetStyle();
+
+    }).get();
+}
+
+d3.select('#run').on('click', run);
